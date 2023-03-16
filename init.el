2a3ad9cb567a7f09f1c7fc4067f3ec9a0e2ee8da
Alessandro Passerini <a.passerini@xelia.it>
2023-03-16 21:56:03 +0100

    Experiments with VSCode Theme

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 (defvar my-config-dir (expand-file-name "config/" user-emacs-directory)
   "This directory houses packages that are not yet available in ELPA (or MELPA).")
 
-(defvar my-init-file (expand-file-name "emacs-init.el" my-config-dir)
+(defvar my-init-file (expand-file-name "emacs-init.elc" my-config-dir)
   "All configurations stored in this file.")
 
 (defvar my-org-file (expand-file-name "emacs-init.org" my-config-dir)
@@ -29,12 +29,16 @@
 ;; No more scrollbars
 (scroll-bar-mode -1)
 
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
 ;; Load compiled Lisp file.
 ;; If this do not exists use the original org file to produce Lisp file.
 (if (file-exists-p my-init-file)
-  (load my-init-file t t)
+  ;; Load compiled config file
+  ;; Arguments means: report error if file not found, do not print loading message, do not add suffix
+  (load my-init-file nil t t)
   (progn
-    (org-babel-load-file my-org-file)
+    (org-babel-load-file my-org-file t)
     )
   )
 
