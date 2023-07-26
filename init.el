95e47aa754c35853d63785fc85ffd388fa8c77c1
Alessandro Passerini <a.passerini@xelia.it>
2023-07-26 16:18:46 +0200

    Move compiler settings in init.el

--- init.el
+++ init.el
@@ -29,7 +29,21 @@
 ;; No more scrollbars
 (scroll-bar-mode -1)
 
+;; Start maximized
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
+;; Native compilation
+;; I strongly suggest to enable  Native Compilation whenever possible.
+(setq native-comp-deferred-compilation t)
+(setq package-native-compile t)
+(setq comp-async-report-warnings-errors nil)
+
+;; Fix compile warnings
+(setq personal-keybindings ())
+
+;; Workaround for (temporary?) Emacs error.
+;; Details can be found here: https://emacs.stackexchange.com/questions/74289/emacs-28-2-error-in-macos-ventura-image-type-invalid-image-type-svg
+(setq image-types (cons 'svg image-types))
 
 ;; Load compiled Lisp file.
 ;; If this do not exists use the original org file to produce Lisp file.
